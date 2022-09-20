0fb9d260fbec54f62f1b46a7497c0f9c0d38ea29
KalkM <106752167+KalkM@users.noreply.github.com>
2022-09-20 20:06:06 +0300

    remove char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-  if n < 0:
+    if n >= 0:
+        newstr = str[:n] + str[n + 1:]
+        return (newstr)
+    else:
         return (str)
-    return (str[:n] + str[n+1:])
