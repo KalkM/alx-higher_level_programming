76ebd4f6be9ff8cf625765fcc6221a1c8965978d
KalkM <106752167+KalkM@users.noreply.github.com>
2022-09-24 09:07:27 +0300

    Print a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end="")
-                if j != (len(matrix[i]) - 1):
-                    print(" ", end="")
-
-        print("")
+    for elm in matrix:
+        print(" ".join("{:d}".format(i) for i in elm))
