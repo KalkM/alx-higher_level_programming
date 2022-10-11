18db186e2e87887d76572ec4823422dce462e6e3
KalkM <106752167+KalkM@users.noreply.github.com>
2022-10-11 16:44:30 +0300

    error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
+# 1-square.py
+# Kalkidan
+"""Define a class Square."""
+
 
 class Square:
-
+    """Represent a square."""
+    
     def __init__(self, size=0):
-     if not isinstance(size, int):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
