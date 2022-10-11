9f92ff3c43f56b8d64762abdf8add8135b48d198
KalkM <106752167+KalkM@users.noreply.github.com>
2022-10-11 16:31:50 +0300

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 0-square.py
 # Kalkidan
-"""Define a class Square."""
+""Define a class Square."""
+
 
 class Square:
-"""Represent a square."""
-pass
-  
+    """Represent a square."""
+    pass
