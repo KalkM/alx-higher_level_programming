ba1d25c51b20b17363808efc09a5b3882dece5b6
KalkM <106752167+KalkM@users.noreply.github.com>
2022-10-26 01:08:32 +0300

    to json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a text file insertion function"""
+"""This module defines a string-to-JSON function"""
+import json
 
 
-def append_after(filename="", search_string="", new_string=""):
-    """Inserts text after each line containing a given string in a file
-    """
-    text = ""
-    with open(filename) as r:
-        for line in r:
-            text += line
-            if search_string in line:
-                text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+def to_json_string(my_obj):
+    """Returns the JSON representation of a string object"""
+    return json.dumps(my_obj)
