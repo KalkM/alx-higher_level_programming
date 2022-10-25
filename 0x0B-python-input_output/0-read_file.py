8ab439a9a8d58f731db12ae2bb58be4a036aa6f4
KalkM <106752167+KalkM@users.noreply.github.com>
2022-10-26 00:23:46 +0300

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""This module defines a text file-reading function"""
 
-"""Defines a text file-reading function."""
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
+    """Prints the contents of a UTF8 text file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
