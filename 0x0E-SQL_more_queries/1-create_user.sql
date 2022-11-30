3ae49736843c7a9521948f42bdc98b36a113ad9b
KalkM <106752167+KalkM@users.noreply.github.com>
2022-11-30 23:50:00 +0300

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+-- Create user 'user_0d_1' with all privileges
+-- Password for user set as 'user_0d_1_pwd'
+-- If user already exists, script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
