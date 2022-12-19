5ab5aa463b067768937c1d3b9c82ce25d32b7a86
KalkM <106752167+KalkM@users.noreply.github.com>
2022-12-19 17:16:02 +0300

    c Pyton Js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar)
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
