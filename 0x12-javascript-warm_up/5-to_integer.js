aca1af93cc70028369c4c817684c165b52a9e0bf
KalkM <106752167+KalkM@users.noreply.github.com>
2022-12-19 16:15:53 +0300

    integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+const argv = process.argv;
+const num = parseInt(argv[2]);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${num}`);
+}
