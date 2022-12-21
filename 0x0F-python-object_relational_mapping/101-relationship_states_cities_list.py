f2dc14134bb7a30ac22d3a30cab8d34911e87157
KalkM <106752167+KalkM@users.noreply.github.com>
2022-12-21 11:20:13 +0300

    advanced

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-'''
-Lists all State objects, and corresponding City objects
-'''
-
-
-from sys import argv
+"""
+Lists all State objects and corresponding City objects contained in the DB
+"""
+import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
@@ -12,17 +10,17 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == '__main__':
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    InstanceSession = sessionmaker(bind=engine)
-    session = InstanceSession()
 
-    states = session.query(State).order_by(State.id).all()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    for state in states:
-        print('{}: {}'.format(state.id, state.name))
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+
+    for state in st:
+        print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print('\t{}: {}'.format(city.id, city.name))
-
-    session.close()
+            print("    {}: {}".format(city.id, city.name))
