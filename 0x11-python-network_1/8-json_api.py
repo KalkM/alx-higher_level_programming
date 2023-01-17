f2ad0ff731cb235e0a6cfeafc8d4afade40ec7af
KalkM <106752167+KalkM@users.noreply.github.com>
2023-01-17 15:25:39 +0300

    json API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""ython script that takes in a URL and an email, sends a POST
+""" python script that takes in a URL and an email, sends a POST
 request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)"""
+and displays the body of the response (decoded in utf-8) """
 
 
 import requests
