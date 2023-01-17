6f1764682bf4d4e947e91664a4222b5a180d08ec
KalkM <106752167+KalkM@users.noreply.github.com>
2023-01-17 21:01:36 +0300

    check

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+"""Takes my Github credentials (username and password)
+and uses the Github API to display my Github id.
 """
-GitHub credentials (username and password) and uses the GitHub API
-to display your id
-"""
-import requests
+
 from sys import argv
+import requests
+
 
 if __name__ == "__main__":
-    r = requests.get('https://api.github.com/user',
-                     auth=(argv[1], argv[2]))
-    if r.status_code == 200:
-        j = r.json()
-        print(j.get('id'))
-    else:
-        print('None')
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(argv[1], argv[2])).json()
+
+    print(req.get('id'))
