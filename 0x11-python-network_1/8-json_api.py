2a5bd1f00aa13d1d12130becf61f2c47063c2bdb
KalkM <106752167+KalkM@users.noreply.github.com>
2023-01-19 20:46:23 +0300

    json api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
-Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
-"""
-import sys
-import requests
-
+'''Python script that takes a leter and posts'''
 
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    import requests
+    import sys
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
-    try:
-        response = r.json()
-        if response == {}:
-            print("No result")
-        else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
-    except ValueError:
+    q = ""
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+
+    res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+
+    res_json = res.json()
+    if res_json == {}:
+        print("No result")
+    elif res_json.get("id") is None or res_json.get("name") is None:
         print("Not a valid JSON")
+    else:
+        print(f"[{res_json.get('id')}] {res_json.get('name')}")
