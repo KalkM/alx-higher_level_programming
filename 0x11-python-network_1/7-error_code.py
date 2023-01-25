af1ecf6af41ade59690f4c07615fae7c66771290
KalkM <106752167+KalkM@users.noreply.github.com>
2023-01-26 01:00:32 +0300

    error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""
-Python script that sends a request to the URL and
-displays:
-- The body of the response if there are no errors
-- The error code when there is an HTTP error.
-"""
+''' HTTP errors '''
 import requests
 import sys
 
-
-if __name__ == "__main__":
-        r = requests.get(sys.argv[1])
-        if r.status_code >= 400:
-            print("Error code: {}".format(r.status_code))
-        else:
-            print(r.text)
+if __name__ == '__main__':
+    req = requests.get(sys.argv[1])
+    if req.status_code > 400:
+        print('Error code:', req.status_code)
+    else:
+        print(req.text)
